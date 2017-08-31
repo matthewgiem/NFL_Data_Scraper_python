e7f02f0525b2b943a505cf495c40444e6937db28
Matthew Giem <matthew.giem@gmail.com>
2017-08-30 18:48:27 -0700

    create the player object with stats

--- draft_helper.py
+++ draft_helper.py
@@ -46,16 +46,21 @@ for item in array:
         player_array = filter(lambda x: x != '\n', player_array)
         new_array.append(player_array)
         player_array = []
-# new_array = list(filter(lambda x: x != '\n', new_array))
+
 # print(new_array[4])
 # print(len(new_array))
 # print(new_array)
 list_of_players = []
 for item in new_array:
-    list_of_players.append(Player(item[0], team = item[2]))
+    list_of_players.append(Player(item[0], team = item[2], bye = item[3], passing_yrd = item[20], passing_td = item[21], passing_int = item[22], rushing_att = item[25], rushing_yrd = item[27], rushing_td = item[27], receiving_trg = item[29], receiving_rec = item[30], receiving_yrd = item[31], receiving_td = item[32], misc_2pt = item[36], fum_lost = item[38], games_played = item[16], games_started = item[17]))
+
+    # (tack_solo = item[], tack_ast = item[], sack = item[], safety = item[], pass_deff = item[], blk_kick = item[], turnover_int = item[], fum_force = item[], fum_recover = item[], defensive_td = item[], fgm_0_19 = item[], fgm_20_29 = item[], fgm_30_39 = item[], fgm_40_49 = item[], fgm_50 = item[], pat = item[],  return_yrd = item[], return_td = item[], age = item[]) items not tracked yet
+
 
 print(list_of_players[1].team)
 print(list_of_players[1].name)
+print(list_of_players[1].bye)
+print(list_of_players[4].name + " " + list_of_players[4].team + " " + list_of_players[4].rushing_att)
 
 
 
